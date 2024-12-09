5173635cc40bd36b1d03cc66999311909a2d9fea
root <root@ax01.localdomain>
2024-12-08 21:44:23 -0500

    Add copy.sh script to copy output

---
+++ copy.sh
@@ -0,0 +1,12 @@
+cp -pr \
+report.html \
+domxss_scan.txt \
+js_var.txt \
+jswordlist.txt \
+jsfiles \
+jslinksecret.txt \
+endpoints.txt \
+live_jsfile_links.txt \
+jsfile_links.txt \
+target.txt \
+/output
